0c75cc4595c62684c2ffed75a6ab1fbeec606deb
nandan-varma <nandanvarma.me@gmail.com>
2023-11-14 00:43:46 +0530

    owners update

--- components/chain.tsx
+++ components/chain.tsx
@@ -10,36 +10,67 @@ function initArray(h: number) {
     return array;
 }
 
+let colors = [
+    "white",
+    "red",
+    "blue",
+    "green",
+]
 
 interface ArrayTableProps { }
 
 interface ArrayTableState {
     array: number[][];
+    owners: number[][];
 }
 
 function ChainGrid() {
     const [h,setH] = useState(8);
     const [array, setArray] = useState<ArrayTableState>({
         array: initArray(h),
+        owners: initArray(h)
     });
+    const [turn,SetTurn] = useState(1);
     useEffect(()=>{
-        setArray({ array: ChainReactionLogic(array.array) });
+        setArray( ChainReactionLogic(array));
     },[array])
     
     function increaseValue(row: number, col: number): void {
-        const newArray = [...array.array];
+        if(array.owners[row][col] != turn){
+            if(array.owners[row][col] == 0){
+                
+            }else{
+                alert( `it is ${colors[turn]}'s turn wait for ${colors[array.owners[row][col]]}'s turn to change this`);
+                return;
+            }
+        }
+
+        const newArray: ArrayTableState = {array: [...array.array],owners: [...array.owners]};
         if(row == 0 || col == 0){
-            if(newArray[row][col] == 2){
+            if(newArray.array[row][col] == 2){
 
             }
         }
-        newArray[row][col] += 1;
-        setArray({ array: ChainReactionLogic(newArray) });
+        newArray.array[row][col] += 1;
+        newArray.owners[row][col] = turn;
+        setArray((prev)=>{
+            SetTurn((prevTurn)=>{
+                let t = (prevTurn+1)%4
+                if(t==0){
+                    return 1;
+                }
+                return t;
+            });
+            return ChainReactionLogic(newArray)
+        }
+
+            );
     }
 
-    function ChainReactionLogic(arr: number[][]): number[][] {
-        const rows = arr.length;
-        const cols = arr[0].length;
+    function ChainReactionLogic(input: ArrayTableState): ArrayTableState {
+        let ans: ArrayTableState = {array: [...input.array],owners: [...input.owners]};
+        const rows = ans.array.length;
+        const cols = ans.array[0].length;
     
         const isValidCell = (r: number, c: number) => r >= 0 && r < rows && c >= 0 && c < cols;
     
@@ -52,10 +83,10 @@ function ChainGrid() {
                 if((r == 0 && c == 0) || (r == rows-1 && c == 0) || (r == 0 && c == cols-1) || (r == rows-1 && c == cols-1) ){
                     limit = 2
                 }
-                if (arr[r][c] >= limit) {
+                if (ans.array[r][c] >= limit) {
                     const distributeValue = 1;
-                    arr[r][c] = 0;
-    
+                    ans.array[r][c] = 0;
+                    
                     const neighbors = [
                         [-1, 0],
                         [1, 0], 
@@ -66,19 +97,21 @@ function ChainGrid() {
                     for (const [dr, dc] of neighbors) {
                         const newRow = r + dr;
                         const newCol = c + dc;
-    
                         if (isValidCell(newRow, newCol)) {
-                            arr[newRow][newCol] += distributeValue;
+                            ans.array[newRow][newCol] += distributeValue;
+                            ans.owners[newRow][newCol] = ans.owners[r][c];
                         }
                     }
+                    ans.owners[r][c] = 0;
                 }
             }
         }
-        return arr;
+        return ans;
     }
 
     return (
         <div className="mt-8">
+            <div className='h-8 w-8 rounded-full' style={{backgroundColor: colors[turn]}}></div>'s turn
             <table className="border-collapse">
                 <tbody>
                     {array.array.map((row, rowIndex) => (
@@ -87,9 +120,11 @@ function ChainGrid() {
                                 <td
                                     key={colIndex}
                                     onClick={() => increaseValue(rowIndex, colIndex)}
-                                    className="select-none border border-secondary-foreground p-2 cursor-pointers hover:bg-secondary-foreground hover:text-secondary"
+                                    className="select-none border border-secondary-foreground p-2 cursor-pointers hover:bg-secondary-foreground hover:text-secondary font-extrabold"
+                                    style={{color: array.owners[rowIndex][colIndex] ? colors[(array.owners[rowIndex][colIndex])] : ""}}
                                 >
                                     {cell}
+                                    {/* .{colors[(array.owners[rowIndex][colIndex])]} */}
                                 </td>
                             ))}
                         </tr>
